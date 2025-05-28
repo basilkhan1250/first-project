40da467ccd39aaba2ed059671a51a18401591dd4
M.BasilKhan <basilkhan1250@gmail.com>
2025-05-28 13:57:05 +0500

    workin on it

--- src/components/BusinessModel.jsx
+++ src/components/BusinessModel.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 import fruits from "../assets/fruits.png"
+import electro from "../assets/electro.png"
+import spray from "../assets/spray.png"
+import electricBike from "../assets/electric-bike.png"
+import recycle from "../assets/recycle-shopper.png"
+
+const BusinessObj = [
+    {
+        img: fruits,
+        line: "Fruits and Vegetables Sale Out",
+    },
+    {
+        img: electro,
+        line: "Electrical Distribution Profit",
+    },
+    {
+        img: recycle,
+        line: "Plastic Recycling",
+    },
+    {
+        img: electricBike,
+        line: "Electrical Mobility Rental",
+    },
+    {
+        img: spray,
+        line: "Mosquito System",
+    },
+]
 
 function BusinessModel() {
     return (
@@ -9,10 +36,14 @@ function BusinessModel() {
                 <hr />
                 <p className="business-para">Crowd-funding ownership of</p>
                 <div className="business-images">
-                    <div>
-                        <img src="" />
-                        <p></p>
-                    </div>
+                    {BusinessObj.map((item, index) => {
+                        return (
+                            <div className="business-inner-images">
+                                <img className="business-img" src={item.img} />
+                                <p></p>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </>
