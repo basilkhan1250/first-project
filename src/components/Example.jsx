df1a0d49bc161567535c200b397f577fa37ae3dd
M.BasilKhan <basilkhan1250@gmail.com>
2025-05-31 14:20:21 +0500

    workin on it

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import arrow from "../assets/arrow.png"
 
 function Example() {
     return (
         <>
             <div className="example">
-                <h1>Example</h1>
+                <div className="example-inner">
+                    <h1 className="example-heading">Example</h1>
+                    <hr />
+                </div>
+                <div>
+                    <div className="example-line">
+                        <p>1 Greenhouse = <span>100 NFT</span><img src={arrow} /> <span>100 USD</span> </p>
+                    </div>
+                    <div>
 
+                    </div>
+                    <div></div>
+                </div>
             </div>
         </>
     )
