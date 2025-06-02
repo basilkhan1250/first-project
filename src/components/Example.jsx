999fa442ad54e4adaa8246e95c36c02e62b33ffd
M.BasilKhan <basilkhan1250@gmail.com>
2025-06-02 15:59:11 +0500

    workin on it

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import arrow from "../assets/arrow.png"
+import people from "../assets/people.png"
+import vegNpanel from "../assets/vegs-panel.png"
+import house from "../assets/house-leaf.png"
 
 function Example() {
     return (
@@ -13,8 +16,18 @@ function Example() {
                     <div className="example-line">
                         <p>1 Greenhouse = <span>100 NFT</span><img src={arrow} /> <span>100 USD</span> Price </p>
                     </div>
-                    <div>
-
+                    <div className="example-second">
+                        <img src={people} />
+                        <div>
+                            <p>Buy</p>
+                            <img src={arrow} />
+                        </div>
+                        <img src={house} />
+                        <div>
+                            <p>Generate</p>
+                            <img src={arrow} />
+                        </div>
+                        <img src={vegNpanel} />
                     </div>
                     <div></div>
                 </div>
